f8e4c44e8b15ae31b400066c75b45f76f7f64c7f
justin-aj <ajinfrankj@gmail.com>
2024-12-16 16:32:11 -0500

    max-heap.py

--- max-heap.py
+++ max-heap.py
@@ -1,18 +1,15 @@
-heap_list = [10, 5, 20, 2, 4, 8, 15]
+heap_list = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
 
 def max_heapify(heap_list, i):
     largest = i
     left = (2 * i) + 1
     right = (2 * i) + 2
-    if (2 * i) + 1 < len(heap_list):
+    if left < len(heap_list) and heap_list[left] > heap_list[largest]:
         largest = left
-    if (2 * i) + 2 < len(heap_list):
+    if right < len(heap_list) and heap_list[right] > heap_list[largest]:
         largest = right
     if largest != i:
-        if heap_list[i] < heap_list[left]:
-            heap_list[i], heap_list[left] = heap_list[left], heap_list[i]
-        elif heap_list[i] < heap_list[right]:
-            heap_list[i], heap_list[right] = heap_list[right], heap_list[i]
+        heap_list[i], heap_list[largest] = heap_list[largest], heap_list[i]
         max_heapify(heap_list, largest)
 
 for i in range(len(heap_list) // 2 - 1, -1, -1):
