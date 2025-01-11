167d2e3d1f168a6fb699600d8e57fdf4f6627d54
justin-aj <ajinfrankj@gmail.com>
2025-01-10 20:33:19 -0500

    productsofarrayexceptself.py

--- googlesheets/Trees/lowestcommonancestor.py
+++ googlesheets/Trees/lowestcommonancestor.py
@@ -7,7 +7,6 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-
         while root:
             if root.val < p.val and root.val < q.val:
                 root = root.right
