2a6ff96375c957c6119885d50841e2b9f979ffd6
justin-aj <ajinfrankj@gmail.com>
2025-01-15 18:52:07 -0500

    longestincreasingsequence.py

--- test.py
+++ test.py
@@ -8,3 +8,21 @@ for i, j in enumerate(a):
     print(i, j)
 
 # Both are same
+
+from bisect import bisect_left, bisect_right, insort, insort_left, insort_right
+
+a = [1, 5, 2, 2]
+e = 5
+
+res = bisect_left(a, e)
+print(res)
+
+res = bisect_right(a, e)
+print(res)
+
+insort_left(a, e)
+print(a)
+
+insort_right(a, e)
+print(a)
+
